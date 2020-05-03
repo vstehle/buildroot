5be0654146507c3a6e736f1e4c8a00ac1586de55
Vincent Stehlé <vincent.stehle@laposte.net>
2020-05-03 17:20:06 +0200

    Hack meson build for libglib2
    Signed-off-by: Vincent Stehlé <vincent.stehle@laposte.net>

--- package/pkg-meson.mk
+++ package/pkg-meson.mk
@@ -84,7 +84,7 @@ define $(2)_CONFIGURE_CMDS
 	PATH=$$(BR_PATH) $$($$(PKG)_CONF_ENV) $$(MESON) \
 		--prefix=/usr \
 		--libdir=lib \
-		--default-library=$(if $(BR2_STATIC_LIBS),static,shared) \
+		--default-library=static \
 		--buildtype=$(if $(BR2_ENABLE_DEBUG),debug,release) \
 		--cross-file=$$($$(PKG)_SRCDIR)/build/cross-compilation.conf \
 		-Dbuild.pkg_config_path=$$(HOST_DIR)/lib/pkgconfig \
