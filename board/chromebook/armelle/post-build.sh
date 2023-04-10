08be90ccc9096edc8cb3561696b66564fb614ab3
Vincent Stehlé <vincent.stehle@laposte.net>
2023-04-10 11:16:35 +0200

    armelle ebbr
    Signed-off-by: Vincent Stehlé <vincent.stehle@laposte.net>

---
+++ board/chromebook/armelle/post-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100' ${TARGET_DIR}/etc/inittab
+fi
