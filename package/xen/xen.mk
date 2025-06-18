9a18b16c352423dd153cb02a025b31f715895973
Vincent Stehlé <vincent.stehle@arm.com>
2025-06-18 11:06:02 +0200

    Xen 4.15.7

--- package/xen/xen.mk
+++ package/xen/xen.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-XEN_VERSION = 4.14.6
+XEN_VERSION = 4.15.7
 XEN_SITE = https://downloads.xenproject.org/release/xen/$(XEN_VERSION)
 XEN_SELINUX_MODULES = systemd udev xen
 XEN_LICENSE = GPL-2.0
 XEN_LICENSE_FILES = COPYING
 XEN_CPE_ID_VENDOR = xen
 XEN_CPE_ID_PREFIX = cpe:2.3:o
-XEN_DEPENDENCIES = host-acpica host-python3
+XEN_DEPENDENCIES = host-acpica host-python3 host-meson host-pkgconf
 
 # Calculate XEN_ARCH
 ifeq ($(ARCH),aarch64)
